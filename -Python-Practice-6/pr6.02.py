8e65b774599e26828ca1ca0937cc6b36e1df9c63
Mankurt0 <183689932+Mankurt0@users.noreply.github.com>
2024-10-24 18:54:38 +0300

    Complete pr6.02.py

--- -Python-Practice-6/pr6.02.py
+++ -Python-Practice-6/pr6.02.py
@@ -7,8 +7,10 @@ answer =  [[0, 0, 0, 0],
 for row in range(len(answer)):
     for elem in range(len(answer[0])):
         answer[row][elem] = matrix1[row][elem] * matrix2[row][elem]
-x = answer[0]
-b = 0
-for a in x:
-    b += a
-print(b)
+sum1 = 0
+sum2 = 0
+for i in answer[0]:
+    sum1 += i
+for i in answer[1]:
+    sum2 += i
+print(f"{answer}\n{answer[0]} сумма строки: {sum1}\n{answer[1]} сумма строки: {sum2}")
