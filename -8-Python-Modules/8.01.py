a734e7ebce90af13e0b0f325c1ed9fa1515fc69d
Mankurt0 <183689932+Mankurt0@users.noreply.github.com>
2024-10-25 18:32:17 +0300

    Complete 8.01.py

---
+++ -8-Python-Modules/8.01.py
@@ -0,0 +1,7 @@
+import math
+
+user = int(input("Введите число: "))
+print(f"Квадратный корень {user} = {math.sqrt(user)}\n"
+      f"Синус {user} = {math.sin(user)}\n"
+      f"Косинус {user} = {math.cos(user)}\n"
+      f"Факториал {user} = {math.factorial(user)}")
